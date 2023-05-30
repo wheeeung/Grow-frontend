baf730447995c414363925c5c625ec7752d67741
wheeeung <whee050916@gmail.com>
2023-05-30 14:43:41 +0900

    feat:: edit signup

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,8 +1,39 @@
 import Header from "../../components/common/header/loginHeader.jsx";
 import { logo } from "../../assets";
 import "../login/index.css"
+import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
 
 const Signup = () => {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    if(!username || !email || !password) {
+      alert('이메일, 비밀번호, 이름을 입력해주세요!!!');
+      return;
+    }
+
+
+    navigate("/login");
+  };
+
   return (
     <div>
       <Header />
@@ -10,11 +41,35 @@ const Signup = () => {
         <div style={box1} />
         <p style={pSytle}>회원가입</p>
         <img src={logo} style={logoStyle} />
-        <form>
-          <input type="email" style={loginInput} placeholder="이메일을 입력해주세요" autoFocus />
-          <input type="password" style={passInput} placeholder="비밀번호을 입력해주세요" />
-          <input type="text" style={nameInput} placeholder="닉네임을 입력해주세요" />
-          <button style={button}>회원가입</button>
+        <form onSubmit={handleSignup}>
+          <input 
+            type="email" 
+            id="email"
+            name="email"
+            value={email}
+            style={loginInput} 
+            onChange={handleEmailChange}
+            placeholder="이메일을 입력해주세요" 
+            autoFocus />
+          <input 
+            type="password" 
+            id="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+            style={passInput} 
+            placeholder="비밀번호을 입력해주세요" />
+          <input 
+            type="text" 
+            id="username"
+            name="username"
+            value={username}
+            onChange={handleUsernameChange}
+            style={nameInput} 
+            placeholder="닉네임을 입력해주세요" />
+          <button style={button} type="submit">
+            회원가입
+          </button>
         </form>
       </div>
     </div>  
